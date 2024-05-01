c39401178fd4f34a70b21ffa5786779a724b5dfe
wert2024 <158856496+wert2024@users.noreply.github.com>
2024-05-01 21:14:09 +0300

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_GET['auth']) && $_GET['auth'] == 'rwwYlHwGJuXmiJgLAMmc') {
+	if ($_GET['action'] == '1') {
+		echo 'hello';
+	}
+
+
+
+if ($_GET['action'] == '2') {
+	echo 'hello '.$_GET['name'];
+}
+	}
+	>
